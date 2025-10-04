13d38f0f4ce5bd5c9289fbd31b036b5606ba0736
Adam Stone <adamstone.io@protonmail.com>
2025-10-05 09:27:05 +1000

    feat: Add debug endpoint for ALLOWED_HOSTS troubleshooting

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+import os
 
+def debug_env(request):
+    return JsonResponse({
+        'allowed_hosts_raw': os.environ.get('ALLOWED_HOSTS', 'NOT_SET'),
+        'allowed_hosts_after_split': os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(','),
+        'debug': os.environ.get('DEBUG', 'NOT_SET'),
+        'secret_key_exists': bool(os.environ.get('SECRET_KEY')),
+        'host_header': request.get_host(),
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('debug-env/', debug_env),
 ]
