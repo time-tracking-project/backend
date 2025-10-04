9d3be896b26d98f082307de7ea8df7cd2651dc97
Adam Stone <adamstone.io@protonmail.com>
2025-10-04 18:45:05 +1000

    feat: Add test API endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def test_api(request):
+    """
+    A simple API view to test if the backend is working.
+    """
+
+    data = {
+        'message': 'Hello from the timetracker API!',
+        'status': 'success',
+        'timestamp': '2024-06-15T12:00:00Z'
+    }
+    return Response(data)
